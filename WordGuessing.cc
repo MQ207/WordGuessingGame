dab028cf7b8c78a3a137fd1791ae19baf1900c26
MinhQuan207 <mqhp000@gmail.com>
2026-08-06 13:07:05 -0700

    Removed comments above program explaining what the program does and added it to the readme

--- WordGuessing.cc
+++ WordGuessing.cc
@@ -1,19 +1,3 @@
-/*
-  Word Guesser! By: Minh-Quan Pham
-    Uses random to pull a random word from a word bank from 
-    which the user can type in chars to reveal letters
-
-  To-Do
-    Fix bug where user types in a letter that has already 
-      been guessed and is in the word which adds to 
-      wordFoundCount and may prematurely end the game
-
-    Add Banner or welcome graphic
-    
-    Expand word bank
-
-*/
-
 #include <iostream>
 #include <vector>
 #include <random>
